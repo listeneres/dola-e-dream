62dfa8df799e972b336e0c0e7f9a6d2313f4075a
hzm <hzm9277@126.com>
2017-04-24 17:47:48 +0800

    新增登陆界面

--- charge_sys/src/router/router.js
+++ charge_sys/src/router/router.js
@@ -2,22 +2,15 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from '../App.vue'
 //yw
-import register from '../components/register/registerComponent.vue';
-import login from '../components/login/loginComponent.vue';
+//import login from '../components/login/loginComponent.vue';
 import goods from '../components/goods/goodsComponent.vue';
 Vue.use(VueRouter)
 var router = new VueRouter({
 	routes: [{
 		path: '/',
 		component: App,
-		children: [{
-				path: '/register',
-				component: register
-			},
+		children: [
 			{
-				path: '/login',
-				component: login
-			},{
 				path:'/goods',
 				component:goods
 			}
